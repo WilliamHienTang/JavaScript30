09f376f8ad0d00236a2f8cf2dec621b7a95e2f19
WilliamHienTang <williamhientang@gmail.com>
2021-10-07 16:20:35 -0700

    completed 19

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,93 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo(){
+	navigator.mediaDevices.getUserMedia({ video: true, audio: false})
+		.then(localMediaStream => {
+ 				video.srcObject = localMediaStream;
+				video.play();
+		})
+		.catch(e => {
+			console.error('Error: ', e);
+		});
+}
+
+function paintToCanvas(){
+	const width = video.videoWidth;
+	const height = video.videoHeight;
+	canvas.width = width;
+	canvas.height = height;
+
+	setInterval(() => {
+		ctx.drawImage(video, 0, 0, width, height);
+		
+		let pixels = ctx.getImageData(0, 0, width, height);
+
+		//redEffect(pixels);
+		
+		//rgbSplit(pixels);
+		//ctx.globalAlpha = 0.8;
+
+		greenScreen(pixels);
+
+		ctx.putImageData(pixels, 0, 0);
+	}, 16);
+}
+
+function redEffect(pixels){
+	for(let i=0; i<pixels.data.length; i+=4){
+		pixels.data[i] += 200; // red
+		pixels.data[i+1] -= 50; // green
+		pixels.data[i+2] *= 0.5; // blue
+	}
+}
+
+function rgbSplit(pixels){
+	for(let i=0; i<pixels.data.length; i+=4){
+	    pixels.data[i-150] = pixels.data[i]; // red
+	    pixels.data[i+500] = pixels.data[i+1]; // green
+	    pixels.data[i-550] = pixels.data[i+2]; // blue
+	}
+}
+
+function greenScreen(pixels) {
+	const levels = {};
+
+	document.querySelectorAll('.rgb input').forEach((input) => {
+		levels[input.name] = input.value;
+	});
+
+	for (i = 0; i < pixels.data.length; i = i + 4) {
+		red = pixels.data[i + 0];
+		green = pixels.data[i + 1];
+		blue = pixels.data[i + 2];
+		alpha = pixels.data[i + 3];
+
+		if (red >= levels.rmin
+			&& green >= levels.gmin
+			&& blue >= levels.bmin
+			&& red <= levels.rmax
+			&& green <= levels.gmax
+			&& blue <= levels.bmax) {
+
+		  	pixels.data[i + 3] = 0;
+		}
+	}
+}
+
+function takePhoto(){
+	snap.currentTime = 0;
+	snap.play();
+
+	const data = canvas.toDataURL('image/jpeg');
+	const link = document.createElement('a');
+	link.href = data;
+	link.setAttribute('download', 'selfie');
+	link.innerHTML = `<img src="${data}" alt="selfie" />"`;
+	strip.insertBefore(link, strip.firstChild);
+}
+
+getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
